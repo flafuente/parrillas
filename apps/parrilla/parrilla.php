47c0631636b580e2065b9d3d3b4ceb06f77f9dc3
gerard.arall@gmail.com <gerard.arall@gmail.com>
2014-10-28 14:09:18 +0100

    Parrillas admin

--- apps/parrilla/parrilla.php
+++ apps/parrilla/parrilla.php
@@ -9,7 +9,7 @@ class parrillaController extends Controller
         $user = Registry::getUser();
         if (!$user->id) {
             Url::redirect(Url::site("login"));
-        } elseif ($user->username != "admin") {
+        } elseif (strstr($user->username, "admin")) {
             Registry::addMessage("Sólo los administradores pueden gestionar la parrilla", "error");
             Url::redirect(Url::site("zG2sH0A7hwdnLNUUQaoU25cm"));
         }
